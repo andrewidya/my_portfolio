41948e791b3ad2ac3864b6857f381c7ee0bd08a8
andrewidya <andrywidyaputra@gmail.com>
2014-12-06 15:58:40 +0700

    modified:   my_portfolio/settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -72,10 +72,23 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+if not os.environ.get("HOME") == '/home/andre/':
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aw_database',
+        'USER': 'andrewidya',
+        'PASSWORD': '324234150satriani',
+        'HOST': ''
     }
 }
 
